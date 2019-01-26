ad28f837c9ba715ff22b2939ab77814eee1ffa61
Indre <indre.vailionyte@moovel.com>
2019-01-26 15:51:38 +0100

    Feat: Create ContactList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ContactList contacts={[
+          { name: 'John' },
+          { name: 'Jane' },
+          { name: 'Joe' },
+        ]} />
+        <ContactList contacts={[
+          { name: 'Merry' },
+          { name: 'Moe' },
+          { name: 'Mike' },
+        ]} />
+        <ContactList contacts={[
+          { name: 'Kate' },
+          { name: 'Karl' },
+          { name: 'Kai' },
+        ]} />
       </div>
     );
   }
